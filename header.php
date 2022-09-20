8efe22857e791b633bf6ff15d52bd51c7d7f40bb
Elsa <elsahvy@gmail.com>
2022-09-19 21:45:54 -0700

    vercel test

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>The Indoor Jungle Project</title>
+    <title>the Indoor Jungle Project</title>
 
     <?php wp_head();?>
      
